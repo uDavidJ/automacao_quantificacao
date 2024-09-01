e36ab4bb39be42bb80791e837bd3ebd1ea2dceaa
pedro meireles <pedrinhobelezaa@gmail.com>
2024-08-31 21:41:24 -0300

    commit para teste da inclusão da ferramenta Scenerr Bilderr. tudo dando certo, pequeno passo para os homens, grande passo às mulehres

--- src/main/java/com/example/automacao_quantificacao/HelloApplication.java
+++ src/main/java/com/example/automacao_quantificacao/HelloApplication.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Abcdefgh");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("PrimeiraTelaFXML.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 402);
+        stage.setTitle("Tela Inicial");
         stage.setScene(scene);
         stage.show();
     }

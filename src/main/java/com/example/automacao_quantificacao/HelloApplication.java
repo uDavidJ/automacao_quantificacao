bc54e0afac7ab182e101a572d34272e5bb6df00a
Davi James <davi.vilela9009@gmail.com>
2024-09-04 08:04:42 -0300

    Começo da interface

--- src/main/java/com/example/automacao_quantificacao/HelloApplication.java
+++ src/main/java/com/example/automacao_quantificacao/HelloApplication.java
@@ -17,8 +17,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("PrimeiraTelaFXML.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 402);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 500);
         stage.setTitle("Tela Inicial");
         stage.setScene(scene);
         stage.show();

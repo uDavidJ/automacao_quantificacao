a601bbc57d4c5869652c5de89007330952817991
Davi James <davi.vilela9009@gmail.com>
2024-09-14 17:11:42 -0300

    alterações na interface

--- src/main/java/com/example/automacao_quantificacao/HelloApplication.java
+++ src/main/java/com/example/automacao_quantificacao/HelloApplication.java
@@ -17,7 +17,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("quantificacao_mh_e_backbone.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 500);
         stage.setTitle("Tela Inicial");
         stage.setScene(scene);
--- src/main/java/com/example/automacao_quantificacao/HelloController.java
+++ src/main/java/com/example/automacao_quantificacao/HelloController.java
@@ -1,14 +1,54 @@
 package com.example.automacao_quantificacao;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button botao_calculo;
+
+    @FXML
+    private CheckBox box_area_reservada;
+
+    @FXML
+    private CheckBox box_reguas_fechamento;
+
+    @FXML
+    private CheckBox box_seg;
+
+    @FXML
+    private CheckBox box_voz;
+
+    @FXML
+    private TextField campo_dados;
+
+    @FXML
+    private TextField campo_dt_malha;
+
+    @FXML
+    private TextField campo_n_pavimentos;
+
+    @FXML
+    private TextField campo_pts_pavimentos;
+
+    @FXML
+    private TextField campo_seg;
+
+    @FXML
+    private TextField campo_voz;
+
+    @FXML
+    private ComboBox<?> categoria;
+
+
+    private void acao_calcular() {
+
+
+
     }
+
 }

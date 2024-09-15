99b7bea1ee1be9ae95e8304132aeb7c117a982fb
Davi James <davi.vilela9009@gmail.com>
2024-09-14 22:29:46 -0300

    funcionalidades da tabela

--- src/main/java/com/example/automacao_quantificacao/QuantificacaoMhController.java
+++ src/main/java/com/example/automacao_quantificacao/QuantificacaoMhController.java
@@ -59,6 +59,11 @@ public class QuantificacaoMhController {
         Integer pav = Integer.parseInt(campo_n_pavimentos.getText());
         Integer patchCords = tomadas;
         Integer malha_horizontal = Integer.parseInt(campo_dt_malha.getText()) * tomadas;
+        Integer PP;
+        Integer OF;
+        Integer bandeja_fixa;
+        String modelo_rack;
+
 
         //Quantificacao das tomadas
         ObjetoColuna ob = new ObjetoColuna("Tomada RJ-45 fêmea", "Unid.",
@@ -78,7 +83,7 @@ public class QuantificacaoMhController {
         if(!(campo_voz.getText() == "" || campo_voz.getText() == "0"))
             patchCords = patchCords - Integer.parseInt(campo_voz.getText());
 
-        ob = new ObjetoColuna("Patch Cords", "Unid.",
+        ob = new ObjetoColuna("Cordão de ligação (Patch cord), (Cor: Azul) (Tamanho: 3M)", "Unid.",
                 patchCords,
                 tomadas*pav);
 
@@ -97,6 +102,53 @@ public class QuantificacaoMhController {
 
         ArmazenaListaObjetosColuna.lista.add(ob);
 
+        //Quantificacao dos patch cables
+        if(!(campo_voz.getText() == "" || campo_voz.getText() == "0")) {
+            ob = new ObjetoColuna("Patch Cable (Cor: Amarelo) (Tamanho: 2M)", "Unid.",
+                    Integer.parseInt(campo_voz.getText()),
+                    Integer.parseInt(campo_voz.getText()) * pav);
+            ArmazenaListaObjetosColuna.lista.add(ob);
+        }
+
+        if(!(campo_seg.getText() == "" || campo_seg.getText() == "0")) {
+            ob = new ObjetoColuna("Patch Cable (Cor: Vermelho) (Tamanho: 2M)", "Unid.",
+                    Integer.parseInt(campo_seg.getText()),
+                    Integer.parseInt(campo_seg.getText()) * pav);
+            ArmazenaListaObjetosColuna.lista.add(ob);
+        }
+
+        if(!(campo_dados.getText() == "" || campo_dados.getText() == "0")) {
+            ob = new ObjetoColuna("Patch Cable (Cor: Azul) (Tamanho: 2M)", "Unid.",
+                    Integer.parseInt(campo_dados.getText()),
+                    Integer.parseInt(campo_dados.getText()) * pav);
+            ArmazenaListaObjetosColuna.lista.add(ob);
+        }
+
+        //Patch Panel
+        if(tomadas%24 != 0)
+            PP = (tomadas/24)+1;
+        else
+            PP = tomadas/24;
+
+        ob = new ObjetoColuna("Painel de telecomunicações 24 portas (Altura: 1U)", "Unid.",
+                PP,
+                PP*pav);
+
+        ArmazenaListaObjetosColuna.lista.add(ob);
+
+        //SwitcheS (mesma quantidade de PP)
+        ob = new ObjetoColuna("Switch 24 portas (Altura: 1U)", "Unid.",
+                PP,
+                PP*pav);
+
+        ArmazenaListaObjetosColuna.lista.add(ob);
+
+        //Organizadores frontais de cabos
+        OF = 2*PP;
+        ob = new ObjetoColuna("Organizador frontal de cabo (Altura: 1U)", "Unid.",
+                OF,
+                OF*pav);
+        ArmazenaListaObjetosColuna.lista.add(ob);
 
         HelloApplication.renderizaTelas("tabela");
 
